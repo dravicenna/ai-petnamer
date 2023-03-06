8c3f736a1161213b834c259beebbc0d86dedef58
Vitaly Bobin <vetbobin@gmail.com>
2023-03-07 00:48:02 +0100

    add static pattern to serve statick files with docker

--- aipetnamer/aipetnamer/urls.py
+++ aipetnamer/aipetnamer/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from petnamer import views
@@ -21,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('generate_names', views.generate_names, name='generate_names'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
